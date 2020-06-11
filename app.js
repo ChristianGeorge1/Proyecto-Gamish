5f80ec0697d00482fc64e64c69fd00cc7e9c5101
rcarrasco3652 <rcarrasco0@ucol.mx>
2020-06-10 22:29:12 -0500

    Inicio de sesion avanzando...

--- app.js
+++ app.js
@@ -7,10 +7,10 @@ const mongoose = require('mongoose');
 const port = 4000; 
 
 
-mongoose.connect('mongodb+srv://admi:admi@gamish-ssidz.mongodb.net/<dbname>?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://admi:admi@gamish-ssidz.mongodb.net/gamish?retryWrites=true&w=majority')
 var Schema = mongoose.Schema; 
 
-//*Schema de juegos
+//* Schema de juegos
 var juegoSchema = new Schema ({
     nombre: String, 
     costo: Number, 
@@ -18,7 +18,19 @@ var juegoSchema = new Schema ({
     plataformas: String, 
     categoria: String, 
     descripcion: String
-})
+});
+
+//* Schema de usuario
+var userSchema = new Schema ({
+   id: Number,
+   Nombre: String,
+   correo: String,
+   contraseña: String,
+   genero: String,
+   domicilio: String,
+   tarjetas: String
+
+});
 
 
 
@@ -26,11 +38,33 @@ var juegoSchema = new Schema ({
 //? Esta madre hace que funcione el ejs
 app.set('view engine', 'ejs');
 
-//?Ruta raíz (Lanzara el login)
+//? Ruta raíz (Lanzara el login)
 app.get('/',(req, res)=>{ 
     res.render('index')
 })
 
+//! Inicio de sesion --> Inicio
+app.post('/logIn', urlencodedParser, (req, res) => {
+    var user = mogose.model('user', userSchema)
+
+    user.find({
+        correo: req.body.correo,
+        contraseña: req.body.contraseña
+    },(err, data) => {
+        if(err){
+            res.send('No se encontro usuario con este correo')
+        }else{
+            res.render('home', data)
+        }
+
+    });
+
+});
+
+//! Inicio de sesion --> Fin
+
+
+//? Insertar juegos --> Inicio
 app.get('/insertarJuegos', urlencodedParser,(req, res) => { 
     res.render('insertarJuego')
 })
@@ -58,7 +92,9 @@ app.post('/insertar',urlencodedParser ,(req, res)=> {
     })
 })
 
-//?Ruta de juegos (Lanzara los juegos)
+//? Insertar juegos --> Inicio
+
+//TODO Ruta de juegos (Lanzara los juegos)
 app.get('/juegos', (req, res) => { 
     var juego = mongoose.model('juegos', juegoSchema)
     
