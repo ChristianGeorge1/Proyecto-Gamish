dfa29664b092e8b1b3cccf78362055abc21f52b1
rcarrasco3652 <rcarrasco0@ucol.mx>
2020-06-13 03:03:00 -0500

    Se agregaron los archivos necesarios para insertar un usuario, se implemento el inicio de sesion PERO hacen falta todas las validaciones y los diseños correspondientes para cada pagina html (Todo el codigo esta documentado menos los archivos ejs)

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const port = 4000;
 mongoose.connect('mongodb+srv://admi:admi@gamish-ssidz.mongodb.net/gamish?retryWrites=true&w=majority')
 var Schema = mongoose.Schema; 
 
+//? Esta madre hace que funcione el ejs
+app.set('view engine', 'ejs');
+
+//? Ruta raíz (Lanzara el login)
+app.get('/',(req, res)=>{ 
+    res.render('index')
+})
+
 //* Schema de juegos
 var juegoSchema = new Schema ({
     nombre: String, 
@@ -22,39 +30,45 @@ var juegoSchema = new Schema ({
 
 //* Schema de usuario
 var userSchema = new Schema ({
-   id: Number,
+   
    Nombre: String,
    correo: String,
    contraseña: String,
+   telefono: Number,
    genero: String,
-   domicilio: String,
-   tarjetas: String
+   domicilio: {
+       estado: String,
+       municipio: String,
+       cp: Number,
+       colonia: String,
+       calle: String,
+       numero: Number
+   },
+   tarjeta: {
+        titular: String,
+        numeroTarjeta: Number,
+        vencimientoMes: Number,
+        vencimientoAno: Number,
+        cvv: Number
+   }
 
 });
 
 
 
 
-//? Esta madre hace que funcione el ejs
-app.set('view engine', 'ejs');
-
-//? Ruta raíz (Lanzara el login)
-app.get('/',(req, res)=>{ 
-    res.render('index')
-})
-
 //! Inicio de sesion --> Inicio
 app.post('/logIn', urlencodedParser, (req, res) => {
-    var user = mogose.model('user', userSchema)
+    var user = mongoose.model('user', userSchema)
 
     user.find({
         correo: req.body.correo,
         contraseña: req.body.contraseña
-    },(err, data) => {
+    },(err, user) => {
         if(err){
             res.send('No se encontro usuario con este correo')
         }else{
-            res.render('home', data)
+            res.render('home', {user})
         }
 
     });
@@ -62,6 +76,56 @@ app.post('/logIn', urlencodedParser, (req, res) => {
 });
 
 //! Inicio de sesion --> Fin
+
+//* Registro usuario --> inicio
+
+app.get('/registroUsuario', (req, res) => {
+    res.render('registroUsuario')
+});
+
+//! Agregar usuario --> inicia
+app.post('/registrarUsuario', urlencodedParser, (req,res) => {
+    
+
+    var user = mongoose.model('user', userSchema);
+    
+
+
+    user({
+
+        Nombre: req.body.nombre,
+        correo: req.body.correo,
+        contraseña: req.body.contraseña,
+        telefono: req.body.telefono,
+        genero: req.body.genero,
+        domicilio: {
+            estado: req.body.estado,
+            municipio: req.body.municipio,
+            cp: req.body.cp,
+            colonia: req.body.colonia,
+            calle: req.body.calle,
+            numero: req.body.numCasa
+        },
+        tarjeta: {
+            titular: req.body.titular,
+            numeroTarjeta: req.body.numTarjeta,
+            vencimientoMes: req.body.venciMes,
+            vencimientoAno: req.body.venciAno,
+            cvv: req.body.cvv
+        }
+    
+    }).save((err)=>{ 
+        if(err){
+            console.log('Hay un error');
+        }else{ 
+            res.send('Guardo correctamente'); //Aqui habra un render al home
+        }
+    })
+    
+})
+
+//* Registro usuario --> Fin
+
 
 
 //? Insertar juegos --> Inicio
